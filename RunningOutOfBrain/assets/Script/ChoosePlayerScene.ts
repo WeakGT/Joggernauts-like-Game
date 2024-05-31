3b78abad2e1f9f8639bd4369fed4323e12ce2f35
WeakGT <jasonhuang921003@gmail.com>
2024-05-31 17:05:32 +0800

    update ChooseRoleScene

--- RunningOutOfBrain/assets/Script/ChoosePlayerScene.ts
+++ RunningOutOfBrain/assets/Script/ChoosePlayerScene.ts
@@ -1,4 +1,4 @@
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class ChoosePlayerScene extends cc.Component {
@@ -29,8 +29,8 @@ export default class ChoosePlayerScene extends cc.Component {
 
     public playerNumber: number = 0;
 
-
     loadNextScene() {
+        cc.sys.localStorage.setItem('playerNumber', this.playerNumber);
         cc.director.loadScene("ChooseRoleScene");
     }
 
@@ -58,49 +58,70 @@ export default class ChoosePlayerScene extends cc.Component {
         if (event.keyCode == cc.macro.KEY.q) {
             if (this.playerOneUnready.active == true && this.playerNumber == 0)
                 this.playerOneReady.active = true, this.playerOneUnready.active = false, this.playerNumber++;
-            else if (this.playerOneReady.active == true  && this.playerNumber == 1)
+            else if (this.playerOneReady.active == true && this.playerNumber == 1)
                 this.playerOneReady.active = false, this.playerOneUnready.active = true, this.playerNumber--;
         }
         if (event.keyCode == cc.macro.KEY.r) {
             if (this.playerTwoUnready.active == true && this.playerNumber == 1)
                 this.playerTwoReady.active = true, this.playerTwoUnready.active = false, this.playerNumber++;
-            else if (this.playerTwoReady.active == true  && this.playerNumber == 2)
+            else if (this.playerTwoReady.active == true && this.playerNumber == 2)
                 this.playerTwoReady.active = false, this.playerTwoUnready.active = true, this.playerNumber--;
         }
         if (event.keyCode == cc.macro.KEY.u) {
-            if (this.playerThreeUnready.active == true  && this.playerNumber == 2)
+            if (this.playerThreeUnready.active == true && this.playerNumber == 2)
                 this.playerThreeReady.active = true, this.playerThreeUnready.active = false, this.playerNumber++;
-            else if (this.playerThreeReady.active == true  && this.playerNumber == 3)
+            else if (this.playerThreeReady.active == true && this.playerNumber == 3)
                 this.playerThreeReady.active = false, this.playerThreeUnready.active = true, this.playerNumber--;
         }
         if (event.keyCode == cc.macro.KEY.p) {
-            if (this.playerFourUnready.active == true  && this.playerNumber == 3)
+            if (this.playerFourUnready.active == true && this.playerNumber == 3)
                 this.playerFourReady.active = true, this.playerFourUnready.active = false, this.playerNumber++;
-            else if (this.playerFourReady.active == true  && this.playerNumber == 4)
+            else if (this.playerFourReady.active == true && this.playerNumber == 4)
                 this.playerFourReady.active = false, this.playerFourUnready.active = true, this.playerNumber--;
         }
     }
 
-    // onKeyUp(event) {}
-
-    // onLoad () {}
-
-    start () {
+    start() {
         cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
-        // cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+        this.playerNumber = parseInt(cc.sys.localStorage.getItem('playerNumber')) || 0;
 
-        this.playerOneReady.active = false;
-        this.playerTwoReady.active = false;
-        this.playerThreeReady.active = false;
-        this.playerFourReady.active = false;
+        if (this.playerNumber == 0) {
+            this.playerOneReady.active = false, this.playerOneUnready.active = true;
+            this.playerTwoReady.active = false, this.playerTwoUnready.active = true;
+            this.playerThreeReady.active = false, this.playerThreeUnready.active = true;
+            this.playerFourReady.active = false, this.playerFourUnready.active = true;
+        }
+        else if (this.playerNumber == 1) {
+            this.playerOneReady.active = true, this.playerOneUnready.active = false;
+            this.playerTwoReady.active = false, this.playerTwoUnready.active = true;
+            this.playerThreeReady.active = false, this.playerThreeUnready.active = true;
+            this.playerFourReady.active = false, this.playerFourUnready.active = true;
+        }
+        else if (this.playerNumber == 2) {
+            this.playerOneReady.active = true, this.playerOneUnready.active = false;
+            this.playerTwoReady.active = true, this.playerTwoUnready.active = false;
+            this.playerThreeReady.active = false, this.playerThreeUnready.active = true;
+            this.playerFourReady.active = false, this.playerFourUnready.active = true;
+        }
+        else if (this.playerNumber == 3) {
+            this.playerOneReady.active = true, this.playerOneUnready.active = false;
+            this.playerTwoReady.active = true, this.playerTwoUnready.active = false;
+            this.playerThreeReady.active = true, this.playerThreeUnready.active = false;
+            this.playerFourReady.active = false, this.playerFourUnready.active = true;
+        }
+        else if (this.playerNumber == 4) {
+            this.playerOneReady.active = true, this.playerOneUnready.active = false;
+            this.playerTwoReady.active = true, this.playerTwoUnready.active = false;
+            this.playerThreeReady.active = true, this.playerThreeUnready.active = false;
+            this.playerFourReady.active = true, this.playerFourUnready.active = false;
+        }
 
         this.initNextButton();
         this.initBackButton();
     }
 
-    update (dt) {
+    update(dt) {
         cc.find("Canvas/PlayerNumber").getComponent(cc.Label).string = String(this.playerNumber);
-        if (this.playerNumber == 0) cc.find("Canvas/NextButton").getComponent(cc.Button).node.active = false;
-        else cc.find("Canvas/NextButton").getComponent(cc.Button).node.active = true;
+        cc.find("Canvas/NextButton").getComponent(cc.Button).node.active = this.playerNumber !== 0;
     }
 }
