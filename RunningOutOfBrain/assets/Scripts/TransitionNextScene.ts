7299cb0a9787b1dc790fbae1fedc48b66535ea67
WeakGT <jasonhuang921003@gmail.com>
2024-06-08 03:50:47 +0800

    add transition videos, add buttons

---
+++ RunningOutOfBrain/assets/Scripts/TransitionNextScene.ts
@@ -0,0 +1,15 @@
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class TransitionNextScene extends cc.Component {
+    
+    @property(cc.String)
+    nextScene: string = "StartScene";
+
+    start() {
+        cc.find("Canvas/VideoPlayer").getComponent(cc.VideoPlayer).play();
+        this.scheduleOnce(() => {
+            cc.director.loadScene(this.nextScene);
+        }, 6);
+    }
+}
